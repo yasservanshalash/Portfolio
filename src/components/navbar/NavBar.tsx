7b51a0e8e00d2b5cae141b587cf6e6629b1f0052
Yasser Shalash <yasservanshalash@gmail.com>
2023-01-22 16:30:04 +0100

    navbar theming

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -12,17 +12,17 @@ import Brightness7Icon from '@mui/icons-material/Brightness7';
 import "./NavBar.css"
 const NavBar = ({clicked, setClicked} : {clicked: boolean, setClicked: Function}) => {
   return (
-    <div className='navbar'>
+    <div className={clicked ? "navbarLight" : "navbarDark"}>
         <Container sx={{display: "flex", justifyContent: "space-between", alignItems: "center", p: 2, color: "black"}}>
             <h1>‎ </h1>
             <div>
-                <ul className='navbar__links'>
+                <ul className={ clicked ? 'navbar__links__light' : 'navbar__links__dark'}>
                 <li className='navbar__link'><HomeIcon fontSize='small'/> Home</li>
                 <li className='navbar__link'><ContentCopyIcon fontSize='small' /> Skills</li>
                 <li className='navbar__link'><BadgeIcon fontSize="small" /> Experience</li>
                 <li className='navbar__link'><SchoolIcon fontSize="small" /> Education</li>
                 <li className='navbar__link'><ContactMailIcon fontSize="small" /> Portfolio</li>
-                { clicked? <Brightness7Icon onClick={() => setClicked(!clicked)}/> : <Brightness5Icon onClick={() => setClicked(!clicked)}/>}
+                { clicked? <Brightness7Icon onClick={() => setClicked(!clicked)} sx={{color: clicked ? "white" : "black"}}/> : <Brightness5Icon onClick={() => setClicked(!clicked)}/>}
 
                 </ul>
             </div>
